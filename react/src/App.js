1f58e70b7b828ac9d664317dce9aa39adcb9c6c6
Hydradox <jaimec326@gmail.com>
2022-06-23 00:29:58 +0200

    Progress made

--- react/src/App.js
+++ react/src/App.js
@@ -1,24 +1,13 @@
-import { useState } from 'react';
 import { Outlet, Link } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 
-
-// Import views
-import Home from './views/Home.js';
-import About from './views/About.js';
-import Users from './views/Users.js';
-
+import Navigation from './components/Navigation';
 
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <Link to="/" element={<Home />}>Home</Link>
-        <Link to="/about" element={<About />}>About</Link>
-        <Link to="/users/5" element={<Users />}>Users</Link>
-      </nav>
+      <Navigation />
 
       <Outlet />
     </div>
